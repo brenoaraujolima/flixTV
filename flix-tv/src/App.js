0de1675443a13a106de4ce3ed955412c21a4d781
breno araujo <breno.bal.araujo@gmail.com>
2020-11-21 17:10:11 -0300

    insercao de informacoes do filme do background inicial

--- flix-tv/src/App.js
+++ flix-tv/src/App.js
@@ -1,21 +1,32 @@
 import React, {useEffect,useState} from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default() => {
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState([]);
 
     useEffect(()=> {
       const loadAll = async () => {
         let list = await Tmdb.getHomeList();
         setMovieList(list);
+        let originals = list.filter(i=>i.slug === 'originals');
+        let randomChosen = Math.floor(Math.random()*originals[0].items.results.length -1);
+        let chosen = originals[0].items.results[randomChosen];
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+        setFeaturedData(chosenInfo);
       }
       loadAll();
     },[])
 
     return (
       <div className="page">
-        Ola,mundo!
+        {
+          <FeaturedMovie item={featuredData} />
+        }
+
         <section className="lists">
           {
             movieList.map((item, key)=> {
